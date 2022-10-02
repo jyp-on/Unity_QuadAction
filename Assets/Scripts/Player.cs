41007f5b94bea7c62f0555773927f0d6d9b67286
mythpoy <okmlnsunok@gmail.com>
2022-10-02 16:33:09 +0900

    수류탄 회전

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,19 @@ public class Player : MonoBehaviour
     public float speed;
     public GameObject[] weapons;
     public bool[] hasWeapons;
+    public GameObject[] grenades;
+    public int hasGranades;
+
+    public int ammo;
+    public int coin;
+    public int health;
+    
+
+    public int maxAmmo;
+    public int maxCoin;
+    public int maxHealth;
+    public int maxHasGranades;
+
     float hAxis;
     float vAxis;
     bool wDown;
@@ -169,10 +182,41 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag == "Item")
+        {
+            Item item = other.GetComponent<Item>();
+            switch(item.type)
+            {
+                case Item.Type.Ammo:
+                    ammo += item.value;
+                    if (ammo > maxAmmo)
+                        ammo = maxAmmo;
+                    break;
+                case Item.Type.Coin:
+                    coin += item.value;
+                    if (coin > maxCoin)
+                        coin = maxCoin;
+                    break;
+                case Item.Type.Heart:
+                    health += item.value;
+                    if (health > maxHealth)
+                        health = maxHealth;
+                    break;
+                case Item.Type.Grenade:
+                    hasGranades += item.value;
+                    if (hasGranades > maxHasGranades)
+                        hasGranades = maxHasGranades;
+                    break;
+            }
+            Destroy(other.gameObject);
+        }
+    }
+
     void OnTriggerStay(Collider other) {
         if(other.tag == "Weapon")
             nearObject = other.gameObject;
-        Debug.Log(nearObject.name);
     }
 
     void OnTriggerExit(Collider other) {
